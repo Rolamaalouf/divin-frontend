848ae7f79b67c20a38b5a06f33038c03e6984322
Rolamaalouf <roula.maalouf@hotmail.fr>
2025-05-08 09:56:43 +0300

    orderPage

--- my-next-tailwind-app/app/checkout/page.js
+++ my-next-tailwind-app/app/checkout/page.js
@@ -1,7 +1,6 @@
 // app/checkout/page.tsx or app/orders/page.tsx
 import { Suspense } from "react";
-import OrdersPage from "../../components/OrdersPage";
-
+import OrdersPage from '../components/OrdersPage'
 export default function OrdersPageWrapper() {
   return (
     <Suspense fallback={<div>Loading checkout...</div>}>
