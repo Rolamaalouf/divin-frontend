57c3e1ed002d27940a9cc99f868431af9fda76de
Rolamaalouf <roula.maalouf@hotmail.fr>
2026-01-26 09:19:13 +0200

    removing change

--- my-next-tailwind-app/next.config.mjs
+++ my-next-tailwind-app/next.config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: 'https://www.divinlb.com',
-        permanent: true, // 301 redirect (SEO-friendly)
-      },
-      {
-        source: '/:path*',
-        destination: 'https://www.divinlb.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
