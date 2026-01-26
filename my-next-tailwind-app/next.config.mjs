0bab66cc0cfcdf62e00d3a69d6081353faf70f40
Rolamaalouf <roula.maalouf@hotmail.fr>
2026-01-26 09:15:51 +0200

    redirecting issue

--- my-next-tailwind-app/next.config.mjs
+++ my-next-tailwind-app/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://www.divinlb.com',
+        permanent: true, // 301 redirect (SEO-friendly)
+      },
+      {
+        source: '/:path*',
+        destination: 'https://www.divinlb.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
