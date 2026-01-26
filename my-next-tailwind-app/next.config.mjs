d064fa10a41974633a9ad340cec527a65681d9c0
Rolamaalouf <roula.maalouf@hotmail.fr>
2026-01-26 09:32:48 +0200

    issue

--- my-next-tailwind-app/next.config.mjs
+++ my-next-tailwind-app/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'divinlb.com',
+          },
+        ],
+        destination: 'https://www.divinlb.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
